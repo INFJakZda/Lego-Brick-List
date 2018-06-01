def6ad2a6c97b10821fa8a45ce2906e4af8544fb
INFJakZda <zdanowski78@gmail.com>
2018-06-01 15:12:07 +0200

    Add database to the project

--- app/src/main/java/com/zdano/lego/MainActivity.kt
+++ app/src/main/java/com/zdano/lego/MainActivity.kt
@@ -5,15 +5,31 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-
+import com.zdano.lego.database.DataBaseHelper
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var db : DataBaseHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
+        db = DataBaseHelper(this)
+        db.createDataBase()
+        db.openDataBase()
+        var archive : String? = null
+        var cursor = db.readableDatabase.query("Inventories" , arrayOf("_id, Name, Active, LastAccessed"), null, null, null, null, "LastAccessed ASC")
+
+        if (cursor.moveToFirst()) {
+            var name = cursor.getString(cursor.getColumnIndex("Name"))
+            textView.text = name
+        }
+        cursor.close()
+        db.close()
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
